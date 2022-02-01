aa56e59b3b1c5a5294f262440273ebf238fbca60
Diyorbek7 <jumaniyozovdiyorbek29@gmail.com>
2022-02-01 17:05:06 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lesson_api_post/src/ui/register/register_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        platform: TargetPlatform.iOS,
+        primarySwatch: Colors.blue,
+      ),
+      home: const RegisterScreen(),
+    );
+  }
+}
---
+++ lib/src/ui/register/register_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Register"),
+      ),
+    );
+  }
+}
